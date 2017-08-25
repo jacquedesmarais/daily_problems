dece267bdfaf07d789c300737e1e14c7ab9da8ee
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-25 15:41:24 -0500

    Friday daily problem solution

---
+++ week_one/friday.rb
@@ -0,0 +1,70 @@
+# Create a Rectangle class with readable width and height attributes
+# and a method to calculate its area.
+
+class Rectangle
+
+  def initialize(input_width, input_height)
+    @width = input_width
+    @height = input_height
+  end
+
+  def width
+    return @width
+  end
+
+  def height
+    return @height
+  end
+
+  def area
+    area = @width * @height
+    return area
+  end
+
+end
+
+
+
+# Driver code - don't touch anything below this line.
+puts "TESTING the Rectange class..."
+puts
+
+rectangle = Rectangle.new(10, 30)
+
+result = rectangle.width
+
+puts "width returned:"
+puts result
+puts
+
+if result == 10
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+result = rectangle.height
+
+puts "height returned:"
+puts result
+puts
+
+if result == 30
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+result = rectangle.area
+
+puts "area returned:"
+puts result
+puts
+
+if result == 300
+  puts "PASS!"
+else
+  puts "F"
+end
