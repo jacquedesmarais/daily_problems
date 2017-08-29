984b8548d93336a8050933dc059230e592b92bcd
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-29 17:58:14 -0500

    solution edits

--- week_two/monday.rb
+++ week_two/monday.rb
@@ -1,18 +1,18 @@
-# movies = [
-#   {id: 1, title: "Die Hard", rating: 4.0},
-#   {id: 2, title: "Bad Boys", rating: 5.0},
-#   {id: 3, title: "The Chamber", rating: 3.0},
-#   {id: 4, title: "Fracture", rating: 2.0}
-# ]
-# four_rating_movies = []
+movies = [
+  {id: 1, title: "Die Hard", rating: 4.0},
+  {id: 2, title: "Bad Boys", rating: 5.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+four_rating_movies = []
 
-# movies.select do |movie|
-#   if movie[:rating] < 4
-#     four_rating_movies <<  movie
-#   end
-# end
+movies.select do |movie|
+  if movie[:rating] < 4
+    four_rating_movies <<  movie
+  end
+end
 
-# p four_rating_movies
+p four_rating_movies
 
 string = "What is this?"
 
@@ -20,7 +20,7 @@ def reverse_array(input)
   reverse_arr = []
   arr = input.split('')
   p arr
-    arr.map { reverse_arr << arr.pop }
+  arr.count.times { reverse_arr << arr.pop }
   p reverse_arr.join
 end
 
