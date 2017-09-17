69e846257b3f37ebda12d9af0ebc7acb8587ac27
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-09-17 08:54:53 -0500

    week five folder and sunday solution

---
+++ week_five/sunday.rb
@@ -0,0 +1,12 @@
+# x = 74
+# if x > 10
+#   result = "big number"
+# else
+#   result = "small number"
+# end
+# puts result
+
+# above code refactored to use ternary operator
+x = 74
+result = x > 10 ? "big number" : "small number"
+puts result
